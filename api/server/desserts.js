2d81b388dc136f6d51a567c277e11b10f68ab4f4
Humoyun Toshmatov <tosmatovhumoun03@gmail.com>
2024-11-11 19:46:11 +0500

    fix problem CORS

--- api/server/desserts.js
+++ api/server/desserts.js
@@ -2,6 +2,12 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
+  // Добавляем заголовки для разрешения CORS
+  res.setHeader('Access-Control-Allow-Origin', '*');  // Разрешить доступ с любого источника
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');  // Разрешить методы
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');  // Разрешить заголовки
+
+  // Обработка запроса
   const filePath = path.resolve('my-api.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
