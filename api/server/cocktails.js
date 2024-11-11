abf7b836f02aecf39d7aa9bd5ca3fe09d75cf861
Humoyun Toshmatov <tosmatovhumoun03@gmail.com>
2024-11-11 18:41:16 +0500

    add endpoints

--- api/server/cocktails.js
+++ api/server/cocktails.js
@@ -1,19 +1,16 @@
-// api/server.js
 import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
-  // Чтение данных из my-api.json
   const filePath = path.resolve('my-api.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       return res.status(500).json({ error: 'Error reading JSON file' });
     }
-
-    // Преобразование строки в JSON
-    const products = JSON.parse(data);
-
-    // Ответ с товарами
-    res.status(200).json(products);
+    
+    const allData = JSON.parse(data);
+    const cocktailsData = allData.cocktails || [];
+    
+    res.status(200).json(cocktailsData);
   });
 }
