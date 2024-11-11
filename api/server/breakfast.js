5548464890da73c42ab0b23783e5b76462d7b992
Humoyun Toshmatov <tosmatovhumoun03@gmail.com>
2024-11-11 20:24:53 +0500

    fix cors breakfast.js

--- api/server/breakfast.js
+++ api/server/breakfast.js
@@ -2,6 +2,15 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
+  // Устанавливаем CORS заголовки
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
   const filePath = path.resolve('my-api.json');
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
